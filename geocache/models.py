1c7e3f4aba4d86aef79941bdfaebf990248b8f07
zed-chi <zed-chi@mail.ru>
2021-09-15 22:45:13 +0300

    verbose name

--- geocache/models.py
+++ geocache/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 
 # Create your models here.
 class PlaceCache(models.Model):
-    address = models.CharField(max_length=100)
-    distance = models.FloatField()
-    last_update = models.DateTimeField(default=timezone.now)
+    address = models.CharField("Адрес", max_length=100)
+    distance = models.FloatField("Расстояние")
+    last_update = models.DateTimeField("Дата последнего обновления", default=timezone.now)
