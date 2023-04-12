e2c2ea9ed72f2370b92aa2b871ed5e08e50e3694
zed-chi <zed-chi@mail.ru>
2023-04-12 21:58:05 +0300

    django-configurations upd

--- star_burger/wsgi.py
+++ star_burger/wsgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "star_burger.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+from configurations.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
