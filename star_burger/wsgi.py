9c126b91886e192f7c655a600036f0b37a17a3e7
zed-chi <zed-chi@mail.ru>
2023-04-12 22:24:44 +0300

    django configurations inject

--- star_burger/wsgi.py
+++ star_burger/wsgi.py
@@ -1,18 +1,17 @@
 """
-WSGI config for Django project.
+WSGI config for qwe project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "star_burger.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
-from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
