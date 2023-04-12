113cc699c0ad29c94e98c4b8dae42e3b504a29d6
zed-chi <zed-chi@mail.ru>
2023-04-12 22:31:43 +0300

    changed sequence to read var from env before import conf

--- star_burger/wsgi.py
+++ star_burger/wsgi.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+from environs import Env
+
+env = Env()
+env.read_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "star_burger.settings")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
