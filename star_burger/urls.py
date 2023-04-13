e166daa6e16a85a5e496eda4cc3c6bdbe0cedd23
zed-chi <zed-chi@mail.ru>
2023-04-13 11:32:27 +0300

    moved static routing to debug case

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -25,11 +25,13 @@ urlpatterns = [
     path("", render, kwargs={"template_name": "index.html"}, name="start_page"),
     path("api/", include("foodcartapp.urls")),
     path("manager/", include("restaurateur.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = [
         path(r"__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
